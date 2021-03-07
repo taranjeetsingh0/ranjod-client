4e5b0406e5fb325b491afc9a711ad93ba34463c5
Taranjeet Singh <67357058+blumedata@users.noreply.github.com>
2021-03-07 12:36:47 +0530

    added sass basic

---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Contact.module.scss';
+export default function Contact() {
+    return (
+        <div className={styles.ContactContainer}>
+            <h1>Contact me</h1>
+            <button className={'button'}>
+                Hello
+            </button>
+        </div>
+    );
+}
